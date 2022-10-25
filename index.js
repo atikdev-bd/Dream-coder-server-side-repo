73449fa4489e976a20fdd643ebf246bdc6b73bba
atikdev-bd <atikdev.bd@gmail.com>
2022-10-25 16:42:32 +0600

    listen port

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 
 const Courses = require("./data/data.json");
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log("server is running on port :", port);
+});
