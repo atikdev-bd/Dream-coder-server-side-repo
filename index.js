d6b0f8f2ae3fb909eb2dc72e2c880716be9b8881
atikdev-bd <atikdev.bd@gmail.com>
2022-10-25 16:41:22 +0600

    set port and require json data

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+const Courses = require("./data/data.json");
